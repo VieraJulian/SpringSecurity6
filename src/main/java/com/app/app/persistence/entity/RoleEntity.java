dae7d92f76cf25492ffacbd942a513151c76f28f
VieraJulian <julianviera2019@hotmail.com>
2024-05-16 12:41:04 -0300

    Creando entidad Role y Permission

---
+++ src/main/java/com/app/app/persistence/entity/RoleEntity.java
@@ -0,0 +1,22 @@
+package com.app.app.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name")
+    @Enumerated(EnumType.STRING)
+    private RoleEnum roleEnum;
+}
