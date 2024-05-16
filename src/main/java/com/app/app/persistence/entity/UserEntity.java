95372e63320efe823bb1fc4713c59494d13f1920
VieraJulian <julianviera2019@hotmail.com>
2024-05-16 12:19:48 -0300

    Creando entidad de usuario

---
+++ src/main/java/com/app/app/persistence/entity/UserEntity.java
@@ -0,0 +1,35 @@
+package com.app.app.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @Column(name = "is_Enabled")
+    private boolean isEnabled;
+
+    @Column(name = "account_No_Expired")
+    private boolean accountNoExpired;
+
+    @Column(name = "account_No_Locked")
+    private boolean accountNoLocked;
+
+    @Column(name = "credential_No_Expired")
+    private boolean credentialNoExpired;
+
+}
